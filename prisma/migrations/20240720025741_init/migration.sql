ed1acd814d87e484401edbd1ca18791fb82cb7af
Ernesto Arellano <ernestoarellano@icloud.com>
2024-07-19 20:00:27 -0700

    generate db

--- prisma/migrations/20240720025741_init/migration.sql
+++ prisma/migrations/20240720025741_init/migration.sql
@@ -1,7 +1,11 @@
 -- CreateTable
 CREATE TABLE "Job" (
-    "id" SERIAL NOT NULL,
+    "id" UUID NOT NULL,
     "company" TEXT NOT NULL,
+    "appliedAt" TIMESTAMP(3) NOT NULL,
+    "rejectedAt" TIMESTAMP(3),
+    "interviewAt" TIMESTAMP(3),
+    "acceptedAt" TIMESTAMP(3),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
